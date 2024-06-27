37439bc056279a5709022330a8de0dbc6a59b11f
Trofimov Vladyslav <suzirie@gmail.com>
2024-06-27 17:42:04 +0100

    Revert "meow"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,14 +4,12 @@ import { WelcomeComponent } from './welcome/welcome.component';
 import { StoreComponent } from './store/store.component';
 import { MemberComponent } from './member/member.component';
 import { ProfileEditComponent } from './profile-edit/profile-edit.component';
-import { ImageModalComponent } from './image-modal/image-modal.component';
- 
+
 const routes: Routes = [
   { path: '', component: WelcomeComponent },
   { path: 'store', component: StoreComponent },
   { path: 'member', component: MemberComponent },
   { path: 'profile', component: ProfileEditComponent },
-  { path: 'image-modal', component: ImageModalComponent },
   { path: '**', redirectTo: '' ,}
 ];
 
--- src/app/image-modal/image-modal.component.ts
+++ src/app/image-modal/image-modal.component.ts
@@ -9,5 +9,69 @@ import { JwtHelperService } from '@auth0/angular-jwt';
   styleUrls: ['./image-modal.component.css']
 })
 export class ImageModalComponent {
-  
+  openModal(_t10: any) {
+    throw new Error('Method not implemented.');
+    }
+    selectedImage: any;
+    closeModal() {
+    throw new Error('Method not implemented.');
+    }
+    isModalOpen: any;
+    
+      constructor(private http: HttpClient,  private jwtHelper: JwtHelperService ) { }
+    
+      images: any[] = [];
+      serverName = 'http://localhost:3000';
+    
+      userData = {
+        name: '',
+        avatar: ''
+      };
+    
+      ngOnInit() {
+        this.sendRequest(null);
+        const token = localStorage.getItem('token');
+        if(!token){
+          return
+        }
+        const decodedToken = this.jwtHelper.decodeToken(token);
+        this.userData.name = decodedToken.name;
+        console.log('decodedToken', decodedToken);
+        if (decodedToken.id) {
+          this.userData.avatar = `http://localhost:3000/avatar?id=${decodedToken.id}`;
+        } else {
+          this.userData.avatar = decodedToken.avatar;
+        }
+    
+        console.log('token', token);
+        console.log('User data', this.userData);
+      }
+    
+      onSelectionChange(event: MatSelectChange) {
+        this.sendRequest(event.value);
+      }
+    
+      handleSelectArts(event: any) {
+        console.log('Selected arts', event);
+        if (event.value || event.value === 0) this.sendRequest(event.value, 'category');
+        if (event.user_id) this.sendRequest(event.user_id, 'user');
+      }
+    
+      sendRequest(id: number | null, type: string = 'category') {
+        let req = type === 'category' ? `?category_id=${id}` : `?user_id=${id}`;
+        if (!id) req = '';
+        this.http.get(`${this.serverName}/images${req}`).subscribe(
+          (response: any) => {
+            this.images = response;
+            this.images = this.images.map((image) => {
+              return {
+                ...image,
+                url: `${this.serverName}/${image.file_path}`,
+              };
+            });
+            console.log('Images', this.images);
+          },
+          (error) => console.error('Request failed', error)
+        );
+      }
 }
